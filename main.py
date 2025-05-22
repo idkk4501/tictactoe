9757beac51ef38c140117a1cc8b7823b35e6c2a9
idkk4501 <justinirvin951@gmail.com>
2025-05-22 15:25:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,10 @@ print('2|_|_|_|')
 print('3|_|_|_|')
 
 combs = 'a1', 'b1', 'c1', 'a2', 'b2', 'c2', 'a3', 'b3', 'c3'
+
+computer = random.choice(combs)
+
+print('Your turn first')
+
+player = input('now where do you want to put your X at? Ex. a1>')
+
